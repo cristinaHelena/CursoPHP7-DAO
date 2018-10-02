10f1d2fb8ace992ec3b0584082e9c2dc7a5fe556
cristinahelena <cristinahelenaguyot@gmail.com>
2018-10-02 13:57:49 -0300

    aaaaaaaaaaaaaaa

--- class/Usuarios - Copia.php
+++ class/Usuarios - Copia.php
@@ -52,62 +52,24 @@ class Usuario {
 
             $row = $results[0];
 
-            $this-> setUsuario($row['idusuario']);
+            $this-> setIdusuario($row['idusuario']);
             $this-> setDeslogin($row['deslogin']);
             $this-> setDessenha($row['dessenha']);
             $this-> setDtCadastro(new Datetime($row['dtcadastro']));       
          }                                
     }
 
-    public static function getList(){
-       
-        $sql = new Sql();
-
-        return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin;");
-
-    }
-
-
-    public static function search($login){
-       
-        $sql = new Sql();
-
-        return $sql->select("SELECT * FROM tb_usuarios where deslogin like :SEARCH ORDER BY deslogin;",array(':SEARCH'=>"%".$login."%"));
-
-    }
-
-    public function login($Login, $password){
-
-        $sql = new Sql();
- 
-        $results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD", array(
-            ":LOGIN" => $Login,
-            ":PASSWORD" => $password
-        ));
-
-        if (count($results) > 0){
-
-           $row = $results[0];
-
-           $this-> setUsuario($row['idusuario']);
-           $this-> setDeslogin($row['deslogin']);
-           $this-> setDessenha($row['dessenha']);
-           $this-> setDtCadastro(new Datetime($row['dtcadastro']));       
-        } else {
-
-            throw new Exception("Login e/ou senha inválidos.");
-        }                                
-
-    }
     public function __toString(){
 
         return json_encode(array(
-            "idusuario"=>$this->getUsuario(),
+            "idusuario"=>$this->getIdusuario(),
             "deslogin"=>$this->getDeslogin(),
             "desenha"=>$this->getDessenha(),
             "dtcadastro"=>$this->getDtCadastro()->format("d/m/Y H:i:s")
         ));
     }
+
+   
 
 }
 
--- index.php
+++ index.php
@@ -6,13 +6,25 @@ require_once("config.php");
 
 //$usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
-$root = new Usuario();
 
-$root->loadById(3);
+//Carrega um Usuario
+//$root = new Usuario();
+//$root->loadById(3);
+//echo $root;
 
-echo $root;
+//Carrega uma lista de Usuarios
+//$lista = Usuario::getList();
+//echo json_encode($lista);
 
+//Carrega uma lista de usuarios buscando pelo login
+//$search = Usuario::search("cri");
+//echo json_encode($search);
 
+//Carrega um usuário usando o login e a senha
+$usuario = new Usuario();
+$usuario->login("root","123456");
+
+echo $usuario;
 
 
  ?>
